c38aefd41a0a8d7db25487c1a2410aed43c329a2
Marshall Brandt <MarshallCB@users.noreply.github.com>
2020-08-03 22:42:26 -0500

    Feature: List possible commands
    If captain has no arguments, prompt a list of all possible actions in the to/ directory

--- index.js
+++ index.js
@@ -2,17 +2,27 @@
 var inquirer = require('inquirer')
 var uid = require('uid')
 var kleur = require('kleur')
+const path = require('path');
+const fs = require('fs')
 
 let question = (o) => ({ name: uid(), ...o })
+
 
 async function start(args){
   var flow;
   if(!args.length){
     // No argument supplied. Ask which command they'd like to do
+    let q = question({
+      type: "list",
+      message: "Choose a command",
+      choices: fs.readdirSync(path.join(process.cwd(),`/to`)).map(f => path.parse(f).name)
+    })
+    let answers = await inquirer.prompt([ q ])
+    flow = require(path.join(process.cwd(), `/to/${answers[q.name]}.js`))()
   } else {
     // We can assume first argument is which command they'd like to do
     let which = args.splice(0,1)
-    flow = require(`./to/${which}.js`)(...args)
+    flow = require(path.join(process.cwd(),`/to/${which}.js`))(...args)
   }
   let val = await flow.next();
 
